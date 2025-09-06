c2c539d32b1d3b74cf680d1f049afd630fa4dcd0
Jatin <jatinrajvani.cg@gmail.com>
2025-09-06 15:09:53 +0530

    Adding a backend for adding new product

--- Backend/modules/products/productsroutes.js
+++ Backend/modules/products/productsroutes.js
@@ -4,5 +4,5 @@ const productController = require("./productcontrollers");
 
 router.get("/populate", productController.populateProducts); // populate static products
 router.get("/getproducts", productController.getProducts);              // get all products
-
+router.post("/addproduct", productController.addProduct);           // add new product
 module.exports = router;
